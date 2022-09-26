3a6ea67b7a09e1aecff6f3b540d9245cfdc0bb2a
Javani <moon.blade4@gmail.com>
2022-09-26 15:29:44 +0600

    arrows for slides in svg format

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,9 @@
 $(document).ready(function(){
-    $('.carousel__inner').slick();
+    $('.carousel__inner').slick({
+            speed: 1200,
+            adaptiveHeight: true,
+            prevArrow:'<button type="button" class="slick-prev"><img src="../img/icons/left.svg"></button>'
+        });
   });
 
 
