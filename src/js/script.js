d7d1bb97eec5ff09683d82cd34324b1dea694f0c
Javani <moon.blade4@gmail.com>
2022-09-29 16:23:14 +0600

    ...

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,10 @@
 $(document).ready(function(){
     $('.carousel__inner').slick({
             speed: 1200,
-            adaptiveHeight: true,
-            prevArrow:'<button type="button" class="slick-prev"><img src="../img/icons/left.svg"></button>'
+            // adaptiveHeight: true,
+            prevArrow:'<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
+            nextArrow:'<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
+            
         });
   });
 
